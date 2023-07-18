3b9fe0a444defea79d68cc4aafb9e359bf4ad6d8
tamryn22 <125568338+tamryn22@users.noreply.github.com>
2023-07-18 12:52:59 +0200

    effects

---
+++ index.js
@@ -0,0 +1,15 @@
+$(document).ready(function ()  {
+
+
+    // // // //.selected event
+    // // // //what happens inside ()effects
+    $("#imgButton").dblclick(function (){
+        $("p").slideUp(1000).slideDown(1000)
+    });
+    // //# for id
+    // //2000=2 sec
+    // // $("#imgButton").click(function (){
+    // //     $("img").slideUp(2000).slideDown(2000);
+    // // });
+});
+
